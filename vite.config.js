1994c4a8ed29d4fca1355a5a762e5bb0a589c1b5
kashifmehmoodtech17-cyber <kashifmehmoodtech17@gmail.com>
2025-12-16 01:38:44 +0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/myportfolio-react/',// GitHub Pages repo name
   plugins: [react(), tailwindcss()],
-  base: '/myportfolio-react/' // GitHub Pages repo name
 })
